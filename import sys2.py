866a6ab100cb087fea50410c5939d099e889f4ae
Halfanonim <143794702+Halfanonim@users.noreply.github.com>
2024-06-15 14:06:05 +0300

    Update import sys2.py
    Задача 4

--- import sys2.py
+++ import sys2.py
@@ -1,35 +1,31 @@
 import sys
-def calc(g):
-    W_Ko=0
-    W_Ks=0
-    D = 0
-    for ge in g:
-        kom,ksm=ge.split()
-        if kom==ksm:
-            D+=1
-        elif (kom=='R' and ksm=="S") or (kom=="S" and ksm=="P") or (kom=="P" and ksm=="R"):
-            W_Ko+=1
-        else:
-            W_Ks+=1
-    totalg=len(g)
-    if W_Ko>W_Ks:
-        winner = "Костя"
-    elif W_Ks>W_Ko:
-        winner="Ксюша"
-    else:
-        winner="Ничья"
-    if totalg>0:
-        perc_W_Kost=(W_Ko/totalg)*100
-        perc_W_Ksu=(W_Ks/totalg)*100
-        perc_D=(D/totalg)*100
-    else:
-        perc_W_Kost=0
-        perc_W_Ksu=0
-        perc_D=0
 
-    print(winner)
-    print(f"W: {perc_W_Kost:.2f}%")
-    print(f"D: {perc_D:.2f}%")
-    print(f"L: {perc_W_Ksu:.2f}%")
-inputd=sys.stdin.read().strip().splitlines()
-calc(inputd)
+def rename_file(file_name):
+    # Разбиваем имя файла по разделителям для извлечения компонентов
+    parts = file_name.split('_')
+    
+    # Получаем год, месяц, день, час, минуту и микросекунды из имени файла
+    year = parts[1][:4]
+    month = parts[1][4:6]
+    day = parts[1][6:8]
+    
+    # Преобразуем числовой месяц в текстовый формат
+    months_dict = {
+        '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', 
+        '05': 'May', '06': 'Jun', '07': 'Jul', '08': 'Aug', 
+        '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
+    }
+    month_text = months_dict[month]
+    
+    # Собираем новое имя файла с учетом названия события и расширения
+    new_file_name = f"{year}_{month_text}_{day}_PYTHON_CONFERENCE.jpg"
+    
+    return new_file_name
+
+# Чтение данных из stdin
+if __name__ == "__main__":
+    input_data = sys.stdin.read().strip().splitlines()
+    
+    for line in input_data:
+        new_name = rename_file(line.strip())
+        print(new_name)
